20c4b940af474e13606c2f3287f8d65d7793e42a
irfan <azharkhairulirfan@bcicentral.com>
2024-10-01 09:23:11 +0700

    try first commit on office laptop

--- cypress/e2e/logique/logique_register.cy.js
+++ cypress/e2e/logique/logique_register.cy.js
@@ -22,6 +22,8 @@ describe ('test by pass auth and upload', ()=> {
         lq.openLogicResPage (Cypress.env('logique'), dataLq.username, dataLq.password);
         // cy.log('Vehicle Option:', dataLq.opVehicle);
         lq.registerNewUcAccount(rd.first_name, rd.last_name, rd.city, rd.idNumber, rd.companyName, rd.address, rd.humanFormat, dataLq.opVehicle, dataLq.purpose, dataLq.province, dataLq.cities, rd.email, dataLq.pass, dataLq.rePass);
+        // tes for first commit 
 
+        //test secound commit
     })
 })
